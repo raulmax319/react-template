0c22e0f4ab6934934dc2f71edafc173c5983bc14
Raul Max <raulmax319@gmail.com>
2023-04-11 17:22:22 -0300

    chore: init

---
+++ src/main/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "main/theme/main.scss";
+import App from "./app";
+
+ReactDOM.render(<App />, document.getElementById("main"));
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/main/index.tsx",
+  output: {
+    path: path.join(__dirname, "public/js"),
+    publicPath: "/public/js",
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".scss"],
+    alias: {
+      main: path.resolve(__dirname, "src/main"),
+    },
+  },
+  devServer: {
+    devMiddleware: {
+      writeToDisk: true,
+    },
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    historyApiFallback: true,
+  },
+  externals: {
+    react: "React",
+    "react-dom": "ReactDOM",
+  },
+  plugins: [new CleanWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
+  },
+};
